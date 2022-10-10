8b8fe703709f748ed7dc8b0b3e40e5b7f966cf0a
Antonio Aguirrre Ivorra <antoni.aguirre@gmail.com>
2022-10-10 22:16:20 +0200

    Re-estrcture

--- data/init.py
+++ data/init.py
@@ -1,29 +1,29 @@
 import pygame
 from resources import *
-import main
+import geometry
 
+if __name__ == "__main__":
+    pygame.init()
 
-pygame.init()
+    if pygame.display.mode_ok((BASE_WIDTH, BASE_HEIGHT), DISPLAY_FLAGS):
+        screen = pygame.display.set_mode((BASE_WIDTH, BASE_HEIGHT), DISPLAY_FLAGS)
 
-if pygame.display.mode_ok((BASE_WIDTH, BASE_HEIGHT), DISPLAY_FLAGS):
-    screen = pygame.display.set_mode((BASE_WIDTH, BASE_HEIGHT), DISPLAY_FLAGS)
+    run = 1
+    pygame_time_clock = pygame.time.Clock()
+    ball1, ball1_rect = geometry.render_ball(200, (60, 30, 50))
 
-run = 1
-pygame_time_clock = pygame.time.Clock()
-ball1, ball1_rect = main.render_ball(200, (60, 30, 50))
+    while run:
+        pygame_events = pygame.event.get()
+        for pygame_event in pygame_events:
 
-while run:
-    pygame_events = pygame.event.get()
-    for pygame_event in pygame_events:
+            if QUIT == pygame_event.type or (KEYDOWN == pygame_event.type and
+                                            pygame_event.key in [K_ESCAPE, K_q]):
+                run = 0
 
-        if QUIT == pygame_event.type or (KEYDOWN == pygame_event.type and
-                                         pygame_event.key in [K_ESCAPE, K_q]):
-            run = 0
+        screen.blit(ball1, ball1_rect)
 
-    screen.blit(ball1, ball1_rect)
-
-    
-    pygame.display.flip()
-    
-    # GAME FPS, ALWAYS LAST
-    pygame_time_clock.tick(40)
+        
+        pygame.display.flip()
+        
+        # GAME FPS, ALWAYS LAST
+        pygame_time_clock.tick(40)
