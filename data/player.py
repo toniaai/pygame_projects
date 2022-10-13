a438a9b6bf340f2ad8cdb58db087c173afb044ca
Antonio Aguirrre Ivorra <antoni.aguirre@gmail.com>
2022-10-13 22:26:35 +0200

    Cleanup

--- data/player.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from resources import *
-
-class Player():
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = pygame.image.load(PLAYER_SPRITE)
-        self.mask = pygame.mask.from_surface(self.image)
--- data/resources.py
+++
@@ -1,8 +0,0 @@
-from pygame.locals import *
-
-BASE_HEIGHT = 480
-BASE_WIDTH = 640
-SCREEN_SIZE = (BASE_WIDTH, BASE_HEIGHT)
-TITLE_CAPTION = 'Test Caption'
-DISPLAY_FLAGS = DOUBLEBUF
-PLAYER_SPRITE = '../resources/image/playe/player_sprite.png'
